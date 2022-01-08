8cc47884b6ed7a0c1789694f683804edd6dd2d74
DaviFarias7 <davic.farias7@gmail.com>
2022-01-08 13:46:31 -0300

    Department entitie class

---
+++ src/Application/Program.java
@@ -0,0 +1,14 @@
+package Application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department (1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
