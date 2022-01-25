fc6e76b7eab98899a54b0cac021b518659d38a81
DaviFarias7 <davic.farias7@gmail.com>
2022-01-25 13:40:19 -0300

    Department DaoJDBC and Program2 implemented

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -36,7 +36,10 @@ public class Department implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
